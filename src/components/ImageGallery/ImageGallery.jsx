c90fc812e846d898689ce896a5705668dc00b488
Oleksandr Kostan <kostan.law@gmail.com>
2022-12-08 22:59:58 +0200

    fech-error

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,11 +1,11 @@
 import { Component } from 'react';
 import css from './ImageGallery.module.css';
-import { toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+// import { toast } from 'react-toastify';
+//   import 'react-toastify/dist/ReactToastify.css';
 
 export class ImageGallery extends Component {
   state = {
-    images: null,
+      images: [],
     error: null,
     status: 'idle',
     page: 1,
@@ -37,9 +37,38 @@ componentDidUpdate(prevProps, prevState) {
   }
 
   render() { 
-    return (<><ul className={css.ImageGallery}>
-  {/* <!-- Набір <li> із зображеннями --> */}
-  </ul></>);
-  }
-};
+const { images, error, status } = this.state;
+
+    if (status === 'idle') {
+      // return <Request>Введите запрос! </Request>;
+    }
+    if (status === 'pending') {
+      // return <Loader />;
+    }
+
+    if (status === 'rejected') {
+      // return <ErrorMessage>{error.message} </ErrorMessage>;
+    }
+    if (status === 'resolved') {
+      return (
+        // <GalleryContainer>
+        //   <ImageGalleryItem images={images} />
+        //   {images.total > 0 ? (
+        //     <Button onClick={this.loadMore} />
+        //   ) : (
+        //     <ErrorMessage>По Вашему запросу ничего не найдено</ErrorMessage>
+        //   )}
+        // </GalleryContainer>
+      // );
+    // }
+  // }
+}
+
+
+
+//     return (<><ul className={css.ImageGallery}>
+//   {/* <!-- Набір <li> із зображеннями --> */}
+//   </ul></>);
+//   }
+// };
 
